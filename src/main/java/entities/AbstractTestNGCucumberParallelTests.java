eff786e8e67236e46064eb3d67fa29cf1eeed9cb
gromoval <gromov-rabota@yandex.ru>
2020-04-13 22:02:21 +0300

    Cucumber runs in multithread!

---
+++ src/main/java/entities/AbstractTestNGCucumberParallelTests.java
@@ -0,0 +1,13 @@
+package entities;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.DataProvider;
+
+public abstract class AbstractTestNGCucumberParallelTests extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
